314d8277971c908404233efeae5f439c397da881
anya-kashchenko <116516411+anya-kashchenko@users.noreply.github.com>
2022-12-01 17:46:54 +0200

    sass

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,15 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+    (() => {
+        document
+          .querySelector(".js-speaker-form")
+          .addEventListener("subnit", (e) => {
+            e.preventDefault();
+
+            new FormData(e.currentTarget).forEach((value, name) =>
+              console.log(`${name}: ${value}`)
+            );
+          });
+      })();
